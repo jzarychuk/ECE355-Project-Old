8283ee7974fb445f254cf32f9850983c548c8571
Jacob Arychuk <jza@uvic.ca>
2024-10-25 15:29:16 -0700

    Implement ADC

--- Project/src/main.c
+++ Project/src/main.c
@@ -30,6 +30,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "diag/trace.h"
+#include "cmsis/cmsis_device.h"
 
 // ----------------------------------------------------------------------------
 //
@@ -51,17 +52,86 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
-int
-main(int argc, char* argv[])
-{
-  // At this stage the system clock should have already been configured
-  // at high speed.
+void myADC_Init(void);
+void myGPIOA_Init(void);
 
-  // Infinite loop
-  while (1)
-    {
-       // Add your code here.
-    }
+/*** Call this function to boost the STM32F0xx clock to 48 MHz ***/
+
+void SystemClock48MHz( void )
+{
+//
+// Disable the PLL
+//
+    RCC->CR &= ~(RCC_CR_PLLON);
+//
+// Wait for the PLL to unlock
+//
+    while (( RCC->CR & RCC_CR_PLLRDY ) != 0 );
+//
+// Configure the PLL for 48-MHz system clock
+//
+    RCC->CFGR = 0x00280000;
+//
+// Enable the PLL
+//
+    RCC->CR |= RCC_CR_PLLON;
+//
+// Wait for the PLL to lock
+//
+    while (( RCC->CR & RCC_CR_PLLRDY ) != RCC_CR_PLLRDY );
+//
+// Switch the processor to the PLL clock source
+//
+    RCC->CFGR = ( RCC->CFGR & (~RCC_CFGR_SW_Msk)) | RCC_CFGR_SW_PLL;
+//
+// Update the system with the new clock frequency
+//
+    SystemCoreClockUpdate();
+
+}
+
+int main(int argc, char* argv[]) {
+
+	SystemClock48MHz();
+	trace_printf("System clock: %u Hz\n", SystemCoreClock);
+
+	myADC_Init();
+	myGPIOA_Init();
+
+	// Infinite loop
+	while(1) {
+		ADC1->CR |= 0x4;  // Set Bit 2 to start conversion process (see "Interfacing" Slide 8)
+		while((ADC1->ISR & 0x2) == 0); // Wait for end of conversion flag (Bit 2) to be set
+		unsigned int adc_value = (ADC1->DR & 0xFFF); // Read low 12 bits from ADC1 data register
+		trace_printf("Value going to ADC from POT: %u\n", adc_value); // Print value (0-4095) (2^12)
+	}
+}
+
+void myADC_Init() {
+
+	// Enable clock
+	RCC->APB2ENR |= 0x200; // Set Bit 9 (see "Interfacing" Slide 15)
+
+	// Configure PA4 and PA5 as analog
+	GPIOA->MODER |= 0xF00; // Set Bit 8-11 (see "I/O" Slide 25)
+
+	// Configure ADC
+	ADC1->CFGR1 &= 0xFFFFFFE7; // Clear Bit 3-4 to choose 12-bit resolution (see "Interfacing" Slide 10)
+	ADC1->CFGR1 &= 0xFFFFFFDF; // Clear Bit 5 for right-aligned data (see "Interfacing" Slide 10)
+	ADC1->CFGR1 |= 0x1000; // Set Bit 12 to overwrite when overrun detected (see "Interfacing" Slide 10)
+	ADC1->CFGR1 |= 0x2000; // Set Bit 13 for continuous conversion mode (see "Interfacing" Slide 10)
+
+	// Select Channel 5 for conversion
+	ADC1->CHSELR |= 0x20;// Set Bit 5 (see "Interfacing" Slide 9)
+
+	// Enable taking as many clock cycles as necessary to get reliable sample of analog signal
+	ADC1->SMPR |= 0x7; //Set Bit 0-2 (see "Interfacing" Slide 9)
+
+	// Enable ADC process (basic initialization)
+	ADC1->CR |= 0x1; // Set Bit 0 (see "Interfacing" Slide 8)
+
+	while(((ADC1->ISR & 0x1) == 0)); // Wait for ADC ready flag to be set
+
 }
 
 #pragma GCC diagnostic pop
